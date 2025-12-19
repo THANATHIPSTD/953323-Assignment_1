74e9e3f5bc034b8bd23a78fd7b46295cad823999
THANATHIPSTD <thanathip_suj@cmu.ac.th>
2025-12-19 11:40:26 +0700

    Add contract and Widget in homepage

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firstapp/pages/calc.dart';
 
 class ContactPage extends StatefulWidget {
   const ContactPage({Key? key}) : super(key: key);
@@ -8,15 +9,56 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  final items = List<String>.generate(20, (i) => "Item $i");
+  final mydata = ["Apple", "Banana", "Papaya"];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Chef Thanathip"),
-          Text("@Avengers Building, 189th Floor"),
-        ],
+   
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contact List"),
+      ),
+      body: ListView.builder(
+        itemCount: items.length, 
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              Navigator.push(
+              context,MaterialPageRoute(builder: (context) => const CalculatePage()),
+              );
+            },
+            leading: const Icon(Icons.heart_broken),
+            title: Text(items[index]), 
+          );
+        },
+        // children: [
+        //   ListTile(
+        //     onTap: () {},
+        //     leading: const Icon(Icons.map),
+        //     title: Text(mydata[0]),
+        //   ),
+          
+        //   const Padding(
+        //     padding: EdgeInsets.all(8.0),
+        //     child: Text("Hello List tile"),
+        //   ),
+          
+        //   const Icon(Icons.local_movies, size: 40),
+
+        //   ListTile(
+        //     onTap: () {},
+        //     leading: const Icon(Icons.phone),
+        //     title: Text(mydata[1]),
+        //   ),
+
+        //   ListTile(
+        //     onTap: () {}, 
+        //     leading: const FlutterLogo(),
+        //     title: Text(mydata[2]),
+        //     subtitle: const Text("Hello Chef"),
+        //   ),
+        // ],
       ),
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,12 +10,57 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Center(
-          child: Text("Home Page")
-        ),
-      ]
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ListView(
+        children: [
+          Center(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                MyBox("What is Flutter?", 
+                "Flutter is an open-source UI software development toolkit created by Google. It is used to develop",
+                "https://fluttergems.dev/media-cards/mesh.png"),
+                SizedBox(height: 20,),
+                MyBox("What is Dart?", 
+                "Dart is a client-optimized programming language for apps on multiple platforms. It is developed by Google and is used to build mobile, desktop, server, and web applications.",
+                "https://www.shutterstock.com/image-vector/light-green-white-background-aesthetic-600nw-2626553197.jpg"),
+                SizedBox(height: 20,),
+                MyBox("What is Computer?", 
+                "A computer is a machine that can be instructed to carry out sequences of arithmetic or logical operations automatically via computer programming.",
+                "https://www.simslifecycle.com/wp-content/uploads/sites/2/2022/01/Electronic-Numerical-Integrator-And-Computer.png"),
+              ],
+            ),
+          ),
+        ]
+      ),
     );
+
   }
+  
+Widget MyBox(String title, String description , String imgUrl) {
+
+      return Container(
+        padding: EdgeInsets.all(15),
+        height: 200,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 255, 193, 7),
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            image: NetworkImage(imgUrl),
+            fit: BoxFit.cover,
+          
+          ),
+        ),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title, style: TextStyle(fontSize: 25, color: Colors.white),),
+              Text(description, style: TextStyle(fontSize: 16 , color: Colors.white),),
+            ],
+          ),
+        
+      );
+    }
 }
